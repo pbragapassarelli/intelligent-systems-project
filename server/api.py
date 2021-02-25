430555933f0892f2dda6457685ce10da21fb062e
Pedro Passarelli <pbragapassarelli@yahoo.com.br>
2021-02-25 19:27:41 -0300

    Final solution

--- server/api.py
+++ server/api.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 
 import os
 import pickle
@@ -8,9 +9,9 @@ import scipy as sp
 import pandas as pd
 
 
+# Loading fitted model and word vetor at application start
 WORD_VECTOR_PATH = os.getenv("WORD_VECTOR_PATH")
 MODEL_PATH = os.getenv("MODEL_PATH")
-
 
 with open(WORD_VECTOR_PATH, 'rb') as f:
     word_vector = pickle.load(f)
@@ -19,11 +20,49 @@ with open(MODEL_PATH, 'rb') as f:
     model = pickle.load(f)
 
 
+# Error messages
+MESSAGE_FAILED_DATAFRAME = 'Failed to create a Dataframe. Check input schema.'
+MESSAGE_FAILED_TO_PARSE_CONCATENATED_TAGS = 'Failed to parse concatenated_tags field. \
+    It is either not a string or is absent on some products.'
+MESSAGE_FAILED_MISSING_FIELDS = 'There are missing fields on all products of the input.'
+MESSAGE_FAILED_TO_PREDICT = 'Could not generate predictions. \
+    Check data types or missing fields on some products of the input.'
+
+
+# Instanciating Flask app
 app = Flask(__name__)
 
+
+# Class to treat errors
+class InvalidUsage(Exception):
+
+    def __init__(self, message, status_code=400, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+
+# Flask error handler
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+# Flask route
 @app.route("/v1/categorize", methods=["POST"])
-def hello_world():
-    df = pd.DataFrame(request.json['products'])
+def categorize():
+    try:
+        df = pd.DataFrame(request.json['products'])
+    except:
+        raise InvalidUsage(MESSAGE_FAILED_DATAFRAME)
 
     dense_columns = [
         'search_page',
@@ -38,18 +77,29 @@ def hello_world():
 
     text_column = 'concatenated_tags'
 
-    text_list = [word for word in df[text_column]]
+    try:
+        text_set = df[text_column]
+        dense_set = df[dense_columns].values
+    except:
+        raise InvalidUsage(MESSAGE_FAILED_MISSING_FIELDS)
 
-    sparse_tags = word_vector.transform(text_list)
+    try:
+        text_list = [word for word in text_set]
+        sparse_tags = word_vector.transform(text_list)
+    except:
+        raise InvalidUsage(MESSAGE_FAILED_TO_PARSE_CONCATENATED_TAGS)
 
     final_set = sp.sparse.hstack(
         (
             sparse_tags,
-            df[dense_columns].values
+            dense_set
         ),
         format='csr'
     )
 
-    results = model.predict(final_set)
+    try:
+        results = model.predict(final_set)
+    except:
+        raise InvalidUsage(MESSAGE_FAILED_TO_PREDICT)
 
     return { "categories": list(results) }
--- server/test_api.py
+++ server/test_api.py
@@ -1,21 +1,152 @@
 from api import app
 from flask import json
 
-def test_hello():
+from api import MESSAGE_FAILED_DATAFRAME, \
+    MESSAGE_FAILED_MISSING_FIELDS, \
+        MESSAGE_FAILED_TO_PARSE_CONCATENATED_TAGS, \
+            MESSAGE_FAILED_TO_PREDICT
+
+import pytest
+import numpy as np
+
+
+# Creating the payload for some products
+correct_product_1 = {
+    "search_page": 2,
+    "position": 4,
+    "price": 23.45,
+    "weight": 50.3,
+    "minimum_quantity": 4,
+    "view_counts": 23,
+    "order_counts": 12,
+    "express_delivery": 0,
+    "concatenated_tags": "teste tags"
+}
+
+correct_product_2 = {
+    "search_page": 12,
+    "position": 1,
+    "price": 543.534,
+    "weight": 423450.00,
+    "minimum_quantity": 120,
+    "view_counts": 4893,
+    "order_counts": 500,
+    "express_delivery": 1,
+    "concatenated_tags": "TESTE tagsd"
+}
+
+missing_field_product = {
+    "search_page": 12,
+    "position": 1,
+    "price": 543.534,
+    "concatenated_tags": "TESTE tagsd"
+}
+
+none_order_counts_product = {
+    "search_page": 12,
+    "position": 1,
+    "price": 543.534,
+    "weight": 423450.00,
+    "minimum_quantity": 120,
+    "view_counts": 4893,
+    "order_counts": np.nan,
+    "express_delivery": 1,
+    "concatenated_tags": "TESTE tagsd"
+}
+
+unparseable_tags_product = {
+    "search_page": 12,
+    "position": 1,
+    "price": 543.534,
+    "weight": 423450.00,
+    "minimum_quantity": 120,
+    "view_counts": 4893,
+    "order_counts": "a",
+    "express_delivery": 1,
+    "concatenated_tags": 45
+}
+
+# Creating some inputs for the tests
+correct_input1 = {
+    "products": [
+        correct_product_1,
+        correct_product_2
+    ]
+}
+
+correct_input2 = {
+    "products": [
+        correct_product_1
+    ]
+}
+
+invalid_schema_input = [
+    correct_product_1,
+    correct_product_2
+]
+
+one_product_missing_fields_input = {
+    "products": [
+        correct_product_1,
+        missing_field_product
+    ]
+}
+
+only_missing_fields_input = {
+    "products": [
+        missing_field_product
+    ]
+}
+
+none_order_counts_input = {
+    "products": [
+        none_order_counts_product
+    ]
+}
+
+unparseable_tags_input = {
+    "products": [
+        unparseable_tags_product
+    ]
+}
+
+
+# Declaring the tuples with (input, expected result)
+list_of_expectations_status_code = [
+    (correct_input1, 200),
+    (correct_input2, 200),
+    (invalid_schema_input, 400),
+    (one_product_missing_fields_input, 400),
+    (only_missing_fields_input, 400),
+    (none_order_counts_input, 400),
+    (unparseable_tags_input, 400)
+]
+
+list_of_expectations_messages = [
+    (invalid_schema_input, MESSAGE_FAILED_DATAFRAME),
+    (one_product_missing_fields_input, MESSAGE_FAILED_TO_PREDICT),
+    (only_missing_fields_input, MESSAGE_FAILED_MISSING_FIELDS),
+    (none_order_counts_input, MESSAGE_FAILED_TO_PREDICT),
+    (unparseable_tags_input, MESSAGE_FAILED_TO_PARSE_CONCATENATED_TAGS)
+]
+
+
+# Making the tests
+@pytest.mark.parametrize("test_input, expected", list_of_expectations_status_code)
+def test_no_products_status_code(test_input, expected):
     response = app.test_client().post(
-        "/hello",
-        data=json.dumps({ "name": "42" }),
+        "/v1/categorize",
+        data=json.dumps(test_input),
         content_type="application/json"
     )
+    assert response.status_code == expected
 
-    assert response.status_code == 400
-
-
-def test_hello2():
+@pytest.mark.parametrize("test_input, expected", list_of_expectations_messages)
+def test_no_products_message(test_input, expected):
     response = app.test_client().post(
-        "/hello",
-        data=json.dumps({ "name": "Willian" }),
+        "/v1/categorize",
+        data=json.dumps(test_input),
         content_type="application/json"
     )
+    assert response.json['message'] == expected
 
-    assert response.status_code == 200
